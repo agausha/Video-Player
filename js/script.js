bd08b71a78a5bb4fbd62bd2a9a0834c23a646d32
Isaac <uniqucode@gmail.com>
2022-09-01 07:16:30 +0100

    Refrencing play-btn id

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ const player = document.querySelector('.player');
 const video = document.querySelector('.video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
