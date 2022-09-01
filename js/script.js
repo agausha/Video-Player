8e5acad2f0377613963cd9b3e289e698fc89f184
Isaac <uniqucode@gmail.com>
2022-09-01 07:18:36 +0100

    Refrencing volume-range class

--- js/script.js
+++ js/script.js
@@ -4,3 +4,4 @@ const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.getElementById('play-btn');
 const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
